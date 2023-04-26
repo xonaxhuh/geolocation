70f3e5ad76c3f9b0bc63dacc0ec238e06221dafe
xonax <93013000+xonaxhuh@users.noreply.github.com>
2023-04-26 18:01:14 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let inn = document.getElementById('geo');
+
+function btn() {
+	navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function showPosition(position) {
+	inn.innerHTML = 'Широта: ' + position.coords.latitude + '<br> Долгота: ' + position.coords.longitude;
+}
+
+function errorHandler() {
+	inn.innerHTML = 'Ошибка';
+}
