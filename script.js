a2a0645b979aa56caefcd599b75522772a42919c
xonax <93013000+xonaxhuh@users.noreply.github.com>
2023-04-26 17:54:30 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let inn = document.getElementById('geo');
-
-function btn() {
-	navigator.geolocation.getCurrentPosition(showPosition);
-}
-
-function showPosition(position) {
-	inn.innerHTML = 'Широта: ' + position.coords.latitude + '<br> Долгота: ' + position.coords.longitude;
-}
